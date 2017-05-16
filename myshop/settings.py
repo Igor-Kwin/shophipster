0179aadb91d3816fc81095e3ed80d6b8e9703cc8
Igor Nosatov <sinevor13@gmail.com>
2017-05-17 00:09:03 +0300

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '$fo2-59h%5jl8dng-$h4frk^718g3l1&2yd1gre*+)v%i(_oy_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hippybangoshop.herokuapp.com']
 
 
 # Application definition
