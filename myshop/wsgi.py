88adf68c4d15c84b7fc44484775f8fd5e5789b5e
Igor Nosatov <sinevor13@gmail.com>
2017-05-16 23:29:47 +0300

    Update wsgi.py

--- myshop/wsgi.py
+++ myshop/wsgi.py
@@ -1,9 +1,11 @@
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myshop.settings")
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myshop.settings")
+
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
